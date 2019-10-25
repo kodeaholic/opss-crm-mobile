d79dce2c3f945c7ca08fd584cc0bc95b0c70bdb7
thais <thainh10791@gmail.com>
2019-10-25 11:01:01 +0700

    remove un use

--- react-scripts-wrapper-paths.js
+++
@@ -1,7 +0,0 @@
-const paths = require('react-scripts/config/paths');
-const path = require('path');
-
-// Change 'dist' here to whatever build directory you need.
-paths.appBuild = path.resolve(paths.appPath, 'dist');
-
-module.exports = paths;
