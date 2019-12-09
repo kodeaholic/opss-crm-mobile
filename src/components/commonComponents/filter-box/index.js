6a1ae7335607dddca7a4dd48e772d57f402bae01
duong.nt <duong.nt@citigo.com.vn>
2019-12-09 13:58:08 +0700

    #frontend update filter

--- src/components/commonComponents/filter-box/index.js
+++ src/components/commonComponents/filter-box/index.js
@@ -32,14 +32,14 @@ class ComboFilterSearch extends Component {
         </div>
         <div className="wrapper-filter-search">
           <div className="wrapper-search">
-            <label className="search-label">&nbsp;</label>
+            {/*<label className="search-label">&nbsp;</label>*/}
             <div className="search">
               {/*<i className="fa fa-search search-icon" aria-hidden="true"/>*/}
               <form action="#" id="search-form" onSubmit={e => {
                 e.preventDefault()
               }} autoComplete="off">
                 <input type="search" className="input-search-filter-box"
-                       placeholder="&#xF002; Nhập tìm kiếm nhanh"
+                       placeholder="&#xF002; Global search ..."
                        defaultValue={this.props.match.params.keyword}
                        onKeyPress={this._handleSearchEnter}
                        name="search" id="search"/>
@@ -49,17 +49,34 @@ class ComboFilterSearch extends Component {
           <div className="wrapper-filter">
             <label className="filter-label">{label}</label>
             <div className="filter">
-              <Select
-                classNamePrefix="react-select"
-                value={defaultValue}
-                options={filters}
-                placeholder={placeHolder}
-                onChange={onChange}
-                isSearchable={this.props.isSearchable}
-                components={{
-                  IndicatorSeparator: () => null
-                }}
-              />
+              <div className="filter-item with-margin-right">
+                {/* Loc theo tinh trang lead */}
+                <Select
+                  classNamePrefix="react-select"
+                  value={defaultValue}
+                  options={filters}
+                  placeholder={placeHolder}
+                  onChange={onChange}
+                  isSearchable={this.props.isSearchable}
+                  components={{
+                    IndicatorSeparator: () => null
+                  }}
+                />
+              </div>
+              <div className="filter-item with-margin-left">
+                {/* Loc theo user */}
+                <Select
+                  classNamePrefix="react-select"
+                  value={defaultValue}
+                  options={filters}
+                  placeholder={placeHolder}
+                  onChange={onChange}
+                  isSearchable={this.props.isSearchable}
+                  components={{
+                    IndicatorSeparator: () => null
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
