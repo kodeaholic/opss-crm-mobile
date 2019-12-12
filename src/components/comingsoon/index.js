1dc7612128a774d2fccd3743cd0886cc8f0c375f
duong.nt <duong.nt@citigo.com.vn>
2019-12-12 20:17:51 +0700

    #feature/comingsoon update UI

--- src/components/comingsoon/index.js
+++ src/components/comingsoon/index.js
@@ -14,9 +14,45 @@ class ComingSoon extends Component {
     this.state = {}
   }
 
+  countdownTimer = () => {
+    const difference = +new Date("2020-01-01") - +new Date();
+    let remaining = "Time's up!";
+
+    if (difference > 0) {
+      const parts = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      };
+
+      remaining = Object.keys(parts)
+        .map(part => {
+          if (!parts[part]) return;
+          return `${parts[part]} ${part}`;
+        })
+        .join(" ");
+    }
+
+    document.getElementById("countdown").innerHTML = remaining;
+  }
+
+  componentDidMount() {
+    this.countdownTimer();
+    let timeInterval = setInterval(this.countdownTimer, 1000)
+    this.setState({timeInterval: timeInterval})
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.timeInterval)
+  }
+
   render(){
     return (
-      <div className="wrapper" style={{textAlign: "center", marginTop: '30px'}}>Tính năng sắp ra mắt trong thời gian sắp tới</div>
+      <div className="wrapper" style={{textAlign: "center", marginTop: '30px'}}>
+        <h6 className="rainbow">Tính năng đang phát triển</h6>
+        <div id="countdown" style={{marginTop: '20px'}}/>
+      </div>
     )
   }
 
