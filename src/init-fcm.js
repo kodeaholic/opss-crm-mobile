965270539bfacb349545be8ff2200dffc1dabea0
duong.nt <duong.nt@citigo.com.vn>
2020-02-04 17:29:55 +0700

    dev#CMB-133-react-firebase add exception handler for firebase

--- src/init-fcm.js
+++ src/init-fcm.js
@@ -1,13 +1,22 @@
-import * as firebase from "firebase/app"
-import "firebase/messaging"
-const initializedFirebaseApp = firebase.initializeApp({
-  // Project Settings => Add Firebase to your web app
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-})
-const messaging = initializedFirebaseApp.messaging()
+import * as firebase from 'firebase/app'
+import 'firebase/messaging'
+
+const init = () => {
+  try {
+    return firebase.initializeApp({
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      appId: process.env.REACT_APP_FIREBASE_APP_ID
+    })
+  }
+  catch (e) {
+    console.log(e.message)
+    return undefined
+  }
+}
+const initializedFirebaseApp = init()
+const messaging = initializedFirebaseApp ? initializedFirebaseApp.messaging() : undefined
 messaging.usePublicVapidKey(
   // Project Settings => Cloud Messaging => Web Push certificates (key pair)
   process.env.REACT_APP_FIREBASE_VAPID_KEY
