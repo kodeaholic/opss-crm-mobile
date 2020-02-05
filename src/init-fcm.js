cb3eaf1999dac3d8efc86cc991249aafe111867c
duong.nt <duong.nt@citigo.com.vn>
2020-02-05 14:00:49 +0700

    dev#CMB-133-react-firebase update exception handler logic

--- src/init-fcm.js
+++ src/init-fcm.js
@@ -3,12 +3,17 @@ import 'firebase/messaging'
 
 const init = () => {
   try {
-    return firebase.initializeApp({
-      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-      appId: process.env.REACT_APP_FIREBASE_APP_ID
-    })
+    if (firebase.messaging.isSupported()) {
+      return firebase.initializeApp({
+        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+        appId: process.env.REACT_APP_FIREBASE_APP_ID
+      })
+    }
+    else {
+      return undefined
+    }
   }
   catch (e) {
     console.log(e.message)
